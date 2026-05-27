5fba19ee111b805d0be38c738c0d0f5162028492
Maiesha Moohan <maieshamoohan@gmail.com>
2026-05-27 14:10:36 +0200

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,13 @@
+function greet(name) {
+  return `Hey, ${name} how are you doing?`;
+}
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+const greeting = greet("Najma");
+const sum = add(7, 11);
+
+console.log(greeting);
+console.log(sum);
